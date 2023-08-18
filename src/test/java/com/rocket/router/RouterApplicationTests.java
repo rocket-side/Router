6a273de779c0e3a24cda3c305c71de8b1dae04a3
ghsyn <swanshawn715@gmail.com>
2023-08-18 17:10:36 +0900

    router init

---
+++ src/test/java/com/rocket/router/RouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
